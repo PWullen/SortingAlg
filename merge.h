2916e0cff52385e961f91c3d7e96cccf5c503270
Peter Wullen <46973667+PWullen@users.noreply.github.com>
2021-09-12 13:35:42 -0700

    Create merge.h

---
+++ merge.h
@@ -0,0 +1,12 @@
+//
+// Created by styxw on 9/1/2021.
+//
+
+#ifndef CSE310PROJECT1_MERGE_H
+#define CSE310PROJECT1_MERGE_H
+void mergeSort(){ //argv[1]
+    std::cout<< "Sorting encoding via Merge Sort" << std::endl; //self check for now
+    return;
+    //using ASCII, co-lexicographical order
+}
+#endif //CSE310PROJECT1_MERGE_H
