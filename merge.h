4c195f2ddd345da170e43debb062f49121d2bbd5
Peter Wullen <46973667+PWullen@users.noreply.github.com>
2022-07-12 13:29:46 -0700

    Update merge.h

--- merge.h
+++ merge.h
@@ -8,5 +8,6 @@ void mergeSort(){ //argv[1]
     std::cout<< "Sorting encoding via Merge Sort" << std::endl; //self check for now
     return;
     //using ASCII, co-lexicographical order
+    //Merge Sort left out as public repository can allow for intellectual dishonesty for future students. 
 }
 #endif //CSE310PROJECT1_MERGE_H
